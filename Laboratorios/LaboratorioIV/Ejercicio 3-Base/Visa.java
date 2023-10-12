d8c93a56331ed1f5d0a8f84b441b42599bfb901e
Isabella Callejas <98382799+IC-03@users.noreply.github.com>
2023-10-12 15:11:24 -0500

    correccion declaracion y redondeo de resultado

--- Laboratorios/LaboratorioIV/Ejercicio 3-Base/Visa.java
+++ Laboratorios/LaboratorioIV/Ejercicio 3-Base/Visa.java
@@ -1,6 +1,6 @@
 public class Visa extends Tarjeta
 {
-    private Descuento diamante;
+    private Descuento diamante = Descuento.DIAMANTE;
 
     public Visa(String numeroDeCuenta, double valorApertura){
         super(numeroDeCuenta,valorApertura);
@@ -8,11 +8,6 @@ public class Visa extends Tarjeta
     }
 
     public double cuotaDeManejo(){
-        if (diamante != null) {
-            return 20000 - diamante.getValorDescontado(getValorApertura());
-        } else {
-            // Manejar el caso donde basico es null
-            return 20000; // Otra acción apropiada en caso de que basico no esté definido.
-        }
+        return  Math.round(20000 - diamante.getValorDescontado(getValorApertura()));
     }
 }
