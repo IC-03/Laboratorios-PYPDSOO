5f59b930b13bb02417637a84a876e6a12c27c002
Isabella Callejas <98382799+IC-03@users.noreply.github.com>
2023-10-12 15:12:50 -0500

    correcion declaracion variable y redondeo de resultado

--- Laboratorios/LaboratorioIV/Ejercicio 3-Base/Nomina.java
+++ Laboratorios/LaboratorioIV/Ejercicio 3-Base/Nomina.java
@@ -1,18 +1,13 @@
 public class Nomina extends Tarjeta
 {
-    private Descuento platino;
+    private Descuento platino = Descuento.PLATINO;
 
     public Nomina(String numeroDeCuenta, double valorApertura){
         super(numeroDeCuenta,valorApertura);
         this.platino = platino;
     }
-    
+
     public double cuotaDeManejo(){
-        if (platino != null) {
-            return 20000 - platino.getValorDescontado(getValorApertura());
-        } else {
-            // Manejar el caso donde platino es null
-            return 20000; // Otra acción apropiada en caso de que basico no esté definido.
-        }
+        return  Math.round(20000 - platino.getValorDescontado(getValorApertura()));
     }
 }
